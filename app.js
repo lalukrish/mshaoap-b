7d63a0bf5b14d08ebead0ba46b8239efdd4bfdde
Lal krishna N C <lalukrishnavallil01@gmail.com>
2021-10-22 11:56:35 +0530

    response send

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const app = express();
 require("dotenv").config();
 
 app.use("/",(req,res,next)=>{
+    
     console.log("hello")
+    res.send("hei")
     next();
+     
 })
 
 app.use("/get",(req,res,next)=>{
